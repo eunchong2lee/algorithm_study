6bd2d54f9d63b9f08bb8c8486857f35efce37536
4hsnim <4hsnim@naver.com>
2022-06-30 09:30:43 +0900

    2022-06-30 임형섭 문제못풀었지만 커밋합니다.(풀이추가)

--- 06.30/임형섭.js
+++ 06.30/임형섭.js
@@ -1,14 +1,24 @@
 // 내가 푼 풀이
 function solution(number, k) {
-  var answer = '';
+  let answer = "";
+  let nums = (number + "").split("");
+  for (let i = 0; i < nums.length - k; i++) {
+    nums(i) + nums();
+  }
+
   return answer;
 }
 
-
-
-
-
-
-
-
 // 정답 풀이
+function solution(number, k) {
+  const arr = [];
+  for (let i = 0; i < number.length; i += 1) {
+    while (arr.length > 0 && arr[arr.length - 1] < number[i] && k > 0) {
+      k -= 1;
+      arr.pop();
+    }
+    arr.push(number[i]);
+  }
+  arr.splice(number.length - k, k);
+  return arr.join("");
+}
