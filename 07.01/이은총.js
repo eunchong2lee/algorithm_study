4084b087d898566673346ef3afee13dc0bbe0bee
eunchong2lee <tlfqjrjs1234@naver.com>
2022-07-01 09:56:22 +0800

    커밋

--- 07.01/이은총.js
+++ 07.01/이은총.js
@@ -35,8 +35,22 @@ console.log(solution(progresses, speeds));
 
 
 
+// 다른 사람 정답 풀이
 
 
+function solution(progresses, speeds) {
+  let answer = [0];
+  let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
+  let maxDay = days[0];
 
+  for(let i = 0, j = 0; i< days.length; i++){
+      if(days[i] <= maxDay) {
+          answer[j] += 1;
+      } else {
+          maxDay = days[i];
+          answer[++j] = 1;
+      }
+  }
 
-// 정답 풀이
+  return answer;
+}
