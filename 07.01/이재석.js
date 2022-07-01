268e7b8f3c38e5ba1ba576ee1ced9bfbf9142e6e
LeeJaeSeok1 <98381653+LeeJaeSeok1@users.noreply.github.com>
2022-07-01 10:21:47 +0900

    커밋

--- 07.01/이재석.js
+++ 07.01/이재석.js
@@ -1,14 +1,27 @@
 // 내가 푼 풀이
 
-
-
-
-
-
-
-
-
-
-
-
 // 정답 풀이
+
+
+function solution(progresses, speeds) {
+    let answer = [];
+    let days = 0;
+    let count = 0;
+  
+    while (progresses.length != 0) {
+      if (progresses[0] + days * speeds[0] >= 100) {
+        progresses.shift();
+        speeds.shift();
+        count += 1;
+      } else {
+        if (count != 0) {
+          answer.push(count);
+          count = 0;
+        }
+        days += 1;
+      }
+    }
+    answer.push(count);
+  
+    return answer;
+  }
