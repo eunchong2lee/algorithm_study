e4a1fe0daff2f2e5dfc75c6d27995f097366c7df
eunchong2lee <tlfqjrjs1234@naver.com>
2022-07-02 08:53:30 +0800

    문제 풀이 못함

--- 07.02/이은총.js
+++ 07.02/이은총.js
@@ -1,10 +1,38 @@
 // 내가 푼 풀이
+let priorities = [1,1,9,1,1,1];
+let location = 2;
 function solution(priorities, location) {
   var answer = 0;
+  let result = priorities.map( (a, i) =>{
+    return [a, i]
+  })
+  var a= [];
+
+
+  // while(true){
+    for(let i =0; i< priorities.length; i++){
+      a.push(result[0]);
+      result.shift();
+      for(let j = 0; j< result.length; j++){
+        if(a[a.length-1][0]<result[j][0]){
+          result.push(a[a.length-1]);
+          a.pop();
+          a.push(result[j]);
+          result.splice(j,1);
+        }
+        console.log(result);
+        console.log(a);
+      }
+      if( result.length ==0){
+        break;
+      }
+
+  }
+  console.log(result);
+  console.log(a);
   return answer;
 }
-
-
+solution(priorities, location);
 
 
 
@@ -15,3 +43,21 @@ function solution(priorities, location) {
 
 
 // 정답 풀이
+
+function solution(priorities, location) {
+    var list = priorities.map((t,i)=>({
+        my : i === location,
+        val : t
+    }));
+    var count = 0;        
+    while(true){
+        var cur = list.splice(0,1)[0];        
+        if(list.some(t=> t.val > cur.val )){
+            list.push(cur);                        
+        }
+        else{            
+            count++;
+            if(cur.my) return count;
+        }
+    }
+}
