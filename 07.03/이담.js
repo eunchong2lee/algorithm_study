7245ff0c821f4583f2d9604af4c5bcda43d60595
damiiya <101298873+damiiya@users.noreply.github.com>
2022-07-04 09:28:46 +0900

    Update 이담.js

--- 07.03/이담.js
+++ 07.03/이담.js
@@ -1,15 +1,14 @@
 // 내가 푼 풀이
 function solution(brown, yellow) {
-  var answer = [];
-  return answer;
+  let answer = [];
+  // 카펫의 세로 길이는 3 이상
+  for (let i = 3; i <= (brown+yellow)/i; i++) {
+    let x = Math.floor((brown+yellow)/i);
+    if( (x-2)*(i-2)=== yellow) {
+      break;
+      }
+  }
+  return [x,i];
 }
-
-
-
-
-
-
-
-
 
 // 정답 풀이
