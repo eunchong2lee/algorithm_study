9c5bb022325c247081ad63f035edb697f28f5a29
LeeJaeSeok1 <98381653+LeeJaeSeok1@users.noreply.github.com>
2022-07-04 10:51:07 +0900

    커밋

--- 07.03/이재석.js
+++ 07.03/이재석.js
@@ -1,15 +1,24 @@
 // 내가 푼 풀이
-function solution(brown, yellow) {
-  var answer = [];
-  return answer;
-}
-
-
-
-
-
 
 
 
 
 // 정답 풀이
+
+function solution(brown, yellow) {
+  var answer = [];
+  let sum = brown + yellow;
+
+  for(let height=3; height<=brown; height++){
+
+      if(sum % height === 0){
+          //가로길이
+          let weight = sum / height;
+
+          if( (height-2) * (weight-2) === yellow){
+              return [weight, height];
+          }
+      }
+  }
+  return answer;
+}
