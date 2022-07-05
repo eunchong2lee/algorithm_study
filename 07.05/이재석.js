6c216dd3cbbb8968d5ddb7e36983bc5ff9c62853
LeeJaeSeok1 <98381653+LeeJaeSeok1@users.noreply.github.com>
2022-07-05 10:43:55 +0900

    커밋

--- 07.05/이재석.js
+++ 07.05/이재석.js
@@ -15,3 +15,22 @@ function solution(numbers) {
 
 
 // 정답 풀이
+
+
+function solution(numbers) {
+  let answer = "";
+  numbers.sort((a, b) => {
+    return Number(String(b) + String(a)) - Number(String(a) + String(b));
+  });
+  numbers.forEach((elem) => {
+    console.log(elem);
+    answer += elem + "";
+  });
+ 
+  console.log(answer);
+  
+  if (answer[0] === "0") {
+    return 0;
+  }
+  return 0;
+}
