f2e5b0faaa91e871244e393e5ae1b87e5940081c
LeeJaeSeok1 <98381653+LeeJaeSeok1@users.noreply.github.com>
2022-07-06 09:57:30 +0900

    커밋

--- 07.06/이재석.js
+++ 07.06/이재석.js
@@ -15,3 +15,23 @@ function solution(numbers, target) {
 
 
 // 정답 풀이
+
+
+function solution(numbers, target) {
+  let answer = 0;
+
+  function dfs(depth, sum) {
+    if (depth === numbers.length) {
+      if (sum === target) {
+        answer += 1;
+      }
+      return;
+    }
+    dfs(depth + 1, sum + numbers[depth]); // 왼쪽
+    dfs(depth + 1, sum - numbers[depth]); // 오른쪽
+  }
+
+  dfs(0, 0);
+
+  return answer;
+}
