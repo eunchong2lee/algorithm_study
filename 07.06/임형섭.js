76e26cc5d2d8ab56321b279c3f9f5deef7be313d
4hsnim <4hsnim@naver.com>
2022-07-06 09:21:21 +0900

    못풀고 정답풀이 추가해서 커밋

--- 07.06/임형섭.js
+++ 07.06/임형섭.js
@@ -4,14 +4,23 @@ function solution(numbers, target) {
   return answer;
 }
 
-
-
-
-
-
-
-
-
-
-
 // 정답 풀이
+function solution(numbers, target) {
+  let answer = 0;
+
+  dfs(0, 0);
+
+  function dfs(index, sum) {
+    if (index === numbers.length) {
+      if (sum === target) {
+        answer++;
+      }
+      return;
+    }
+
+    dfs(index + 1, sum + numbers[index]);
+    dfs(index + 1, sum - numbers[index]);
+  }
+
+  return answer;
+}
