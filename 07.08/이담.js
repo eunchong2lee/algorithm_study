deb0c3df16d703258f8800700dba00025546397c
damiiya <101298873+damiiya@users.noreply.github.com>
2022-07-08 09:20:38 +0900

    Update 이담.js

--- 07.08/이담.js
+++ 07.08/이담.js
@@ -1,9 +1,9 @@
 // 내가 푼 풀이
+// charAt(i): i번째의 문자를 가져옴
+// substring(i): 문자열의 i번째 문자부터 끝까지 가져옴
+
 function solution(s) {
-  var answer = '';
-  return answer;
+    return s.split(" ").map(v => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase()).join(" ");
 }
-
-
 
 // 정답 풀이
