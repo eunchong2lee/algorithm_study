cba2fb407de01dada143777c74c55474c450499c
lojy4231 <lojy4231@gmail.com>
2022-07-08 09:21:32 +0900

    정현 풀이

--- 07.08/임정현.js
+++ 07.08/임정현.js
@@ -1,9 +1,17 @@
 // 내가 푼 풀이
 function solution(s) {
-    var answer = '';
-    return answer;
-  }
-  
-  
-  
+  s = s.toLowerCase()
+  var answer = s.split(" ").map(s => {
+    let arr = s.split("")
+    if (arr[0] != null) arr[0] = arr[0].toUpperCase();
+    return arr.join('');
+  }).join(' ');
+  return answer;
+}
+
+
+
+console.log(solution("3people unFollowed me"))
+console.log(solution("for the last week"))
+
   // 정답 풀이
