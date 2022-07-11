05e6effd48452fd5edb89ed0f93110c280358474
LeeJaeSeok1 <98381653+LeeJaeSeok1@users.noreply.github.com>
2022-07-11 09:29:02 +0900

    커밋

--- 07.11/이재석.js
+++ 07.11/이재석.js
@@ -6,12 +6,26 @@ function solution(files) {
 
 
 
-
-
-
-
-
-
-
-
 // 정답 풀이
+
+function solution(files) {
+  let answerWrap = files.map((file, index) => ({file, index}));
+  const compare = (a, b) => {
+    const reg = /(\D*)(\d*)/i;
+    const A = a.match(reg);
+    const B = b.match(reg);
+    const compareHead = A[1].toLowerCase().localeCompare(B[1].toLowerCase());
+    const compareNumber = (a, b) => {
+       return parseInt(a) > parseInt(b) ? 
+          1 : parseInt(a) < parseInt(b) ? 
+           -1 
+          : 0
+    }
+    return compareHead === 0 ? compareNumber(A[2], B[2]) : compareHead
+  }
+  answerWrap.sort((a, b) => {
+    const result = compare(a.file, b.file);
+    return result === 0 ? a.index - b.index : result;
+  })
+  return answerWrap.map(answer => answer.file);
+}
