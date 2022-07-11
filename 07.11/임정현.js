79649f09979c6193f4048bd071ec1b88121751f5
lojy4231 <lojy4231@gmail.com>
2022-07-11 09:27:01 +0900

    정현풀이: 이거 너무 어려운거 아냐???

--- 07.11/임정현.js
+++ 07.11/임정현.js
@@ -1,17 +1,45 @@
 // 내가 푼 풀이
 function solution(files) {
-    var answer = [];
-    return answer;
+  files.sort((a, b) => {
+    const first = tokenize(a);
+    const second = tokenize(b);
+    return fileSort(first, second);
+  });
+  return files;
+}
+
+function tokenize(file) {
+  let startNumIdx;
+  let endNumIdx;
+  for (let i = 0; i < file.length; i++) {
+    if (!startNumIdx && isNumber(file[i])) {
+      startNumIdx = i;
+    }
+    if (startNumIdx && !isNumber(file[i + 1])) {
+      endNumIdx = i;
+      break;
+    }
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  const head = file.slice(0, startNumIdx);
+  const num = file.slice(startNumIdx, endNumIdx + 1);
+  //비교해주기 위해 문자열은 소문자로 숫자string은 숫자로 만들어준다.
+  return [head.toLowerCase(), num * 1];
+}
+function isNumber(char) {
+  return !isNaN(parseInt(char));
+}
+
+
+function fileSort(first, second) {
+  let [firstHead, firstNum] = first;
+  let [secondHead, secondNum] = second;
+  if (firstHead < secondHead) {
+    return -1;
+  } else if (firstHead > secondHead) {
+    return 1;
+  } else return firstNum - secondNum;
+}
+
+
+
   // 정답 풀이
