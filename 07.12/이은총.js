fa81d81cebc26078ed6d631b8106a2e8bcee07f1
eunchong2lee <tlfqjrjs1234@naver.com>
2022-07-12 08:12:56 +0800

    문제 풀이

--- 07.12/이은총.js
+++ 07.12/이은총.js
@@ -1,13 +1,12 @@
 // 내가 푼 풀이
 function solution(record) {
   var answer = [];
+  let middle = record.map(x => x.split(' '))
+  console.log(middle);
   return answer;
 }
-
-
-
-
-
+let record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+solution(record);
 
 
 
@@ -15,3 +14,18 @@ function solution(record) {
 
 
 // 정답 풀이
+
+function solution(record) {
+  var answer = [];
+  const users = {}
+  record.map(history => {
+      const [action, id, name] = history.split(' ')
+      if(action !== 'Leave') users[id] = name
+  })
+  record.map(history => {
+      const [action, id, name] = history.split(' ')
+      if(action === 'Enter') answer.push(`${users[id]}님이 들어왔습니다.`)
+      if(action === 'Leave') answer.push(`${users[id]}님이 나갔습니다.`)
+  })
+  return answer;
+}
