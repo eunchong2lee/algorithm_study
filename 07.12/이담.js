fa6cae18dcf97547d718186dd432dd7879e5017d
damiiya <101298873+damiiya@users.noreply.github.com>
2022-07-12 09:23:41 +0900

    Update 이담.js

--- 07.12/이담.js
+++ 07.12/이담.js
@@ -1,17 +1,23 @@
 // 내가 푼 풀이
+
 function solution(record) {
-  var answer = [];
-  return answer;
+  const userInfo = [];
+  const map = new Map();
+  
+  for (let i = 0; i < record.length; ++i) {
+    const [state, uid, name] = record[i].split(' ');
+    
+    if (state == 'Leave') {
+      userInfo.push([uid, '님이 나갔습니다.']);
+      
+      continue;
+    }
+    if (state == 'Enter') {
+      userInfo.push([uid, '님이 들어왔습니다.']);
+    }
+    map.set(uid, name);
+  }
+  return userInfo.map(ele => map.get(ele[0]) + ele[1]);
 }
-
-
-
-
-
-
-
-
-
-
 
 // 정답 풀이
